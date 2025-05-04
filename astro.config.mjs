410f12cec23577bd071e9f89c6072564cc8ec657
Ollie Reardon <ojreardon@gmail.com>
2025-05-04 10:58:08 +0100

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,10 @@ import tailwind from '@astrojs/tailwind';
 
 import netlify from '@astrojs/netlify';
 
+import alpinejs from '@astrojs/alpinejs';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), alpinejs()],
   adapter: netlify()
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,29 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				gradient: {
+				  '0%': { backgroundPosition: '0% 50%' },
+				  '50%': { backgroundPosition: '100% 50%' },
+				  '100%': { backgroundPosition: '0% 50%' },
+				},
+			  },
+			  animation: {
+				gradient: 'gradient 6s ease infinite',
+			  },
+			colors: {
+				royalPlum: "#6C0080", // Royal Plum (Purple)
+        		sunsetGold: "#FAB32E", // Sunset Gold (Orange)
+			  },
+			  fontFamily: {
+				sans: ['Nunito Sans', 'sans-serif'], // Default sans-serif font
+				quicksand: ['Quicksand', 'sans-serif'], // Add Quicksand font
+			  },
+			  borderRadius: {
+				'lumen': '20px'
+			  }
+		},
 	},
 	plugins: [],
 }
